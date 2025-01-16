22e1562d5e2c9200573b9c8355dd7c6434648b45
VladikNV250 <vladiknemichev@gmail.com>
2025-01-16 21:41:45 +0200

    init

---
+++ vite.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import svgr from "vite-plugin-svgr";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    svgr()
+  ],
+  resolve: {
+    alias: {
+      app: "/src/app",
+      pages: "/src/pages",
+      widgets: "/src/widgets",
+      features: "/src/features",
+      entities: "/src/entities",
+      shared: "/src/shared",
+    }
+  }
+})
